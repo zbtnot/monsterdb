ce933691fe573edcf3d9a7a5d27f1a56f4c56a78
Giorgio Arcamone <giorgio@gioarc.me>
2023-08-30 16:36:18 -0400

    Add header

--- js/components/Layout.vue
+++ js/components/Layout.vue
@@ -1,5 +1,14 @@
 <template>
   <main>
+      <header>
+          <nav
+              class="mx-auto border-b border-slate-900 bg-gray-700 font-serif font-bold"
+          >
+              <RouterLink class="m-2 text-2xl" to="/">
+                  Monster DB
+              </RouterLink>
+          </nav>
+      </header>
     <slot />
   </main>
 </template>
