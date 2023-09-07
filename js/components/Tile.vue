683348138463bda3101030f556ced46851848ac8
Giorgio Arcamone <giorgio@gioarc.me>
2023-09-07 09:53:43 -0400

    Add skeleton to Tile component

--- js/components/Tile.vue
+++ js/components/Tile.vue
@@ -2,21 +2,42 @@
     <router-link
         :to="`/monster/${dexId}`"
         class="m-2 rounded border-2 border-slate-400 bg-slate-600 hover:cursor-pointer hover:bg-slate-700"
+        :class="{ 'animate-pulse': !loaded }"
     >
-        <img
-            class="mt-2"
-            :src="illustrationPath"
-            alt="an illustration of a pokemon"
-        />
-        <div class="bg-slate-800 text-center">
-            {{ name }}
+        <div :class="{ hidden: !loaded }">
+            <img
+                v-if="loaded"
+                :src="illustration"
+                class="mt-2"
+                alt="an illustration of a pokemon"
+            />
+            <div class="bg-slate-800 text-center">
+                {{ name }}
+            </div>
         </div>
     </router-link>
 </template>
 
-<script>
-export default {
-    name: 'Tile',
-    props: ['name', 'dexId', 'illustrationPath'],
-};
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+let props = defineProps({
+    name: String,
+    dexId: Number,
+    illustrationPath: String,
+});
+
+let loaded = ref(false);
+let illustration = ref('');
+
+onMounted(async () => {
+    const illustrationRaw = await axios.get(props.illustrationPath, {
+        responseType: 'arraybuffer',
+    });
+    const illustrationBytes = new Uint8Array(illustrationRaw.data);
+    const base64reducer = (data, byte) => data + String.fromCharCode(byte);
+    illustration.value = 'data:image/png;base64, ' + btoa(illustrationBytes.reduce(base64reducer, ''));
+    loaded.value = true;
+});
 </script>
